a8c4fbaf01c5fbde84eff980cb208cb9c029764a
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-25 02:36:03 -0500

    most components created and coded

---
+++ src/components/MovieScreen.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import MovieCard from "./MovieCard";
+
+const MovieScreen =(props)=>{
+
+    const {movieList, addMovie, setPage, page, list, removeMovie} = props
+
+    const decrement =()=>{
+        setPage(page - 1);
+    };
+
+    const increment =()=>{
+        setPage(page + 1);
+    };
+
+
+    const movieDisplay = movieList.map((movie, index)=>{
+        return(
+            <MovieCard addMovie={addMovie} movie={movie} list={list} removeMovie={removeMovie} />
+        )
+    });
+   
+    return(
+        <div className="page">
+            <h1>Chaltu's Movie Theatre</h1>
+            <h3>Add a movie to your watchlist!</h3>
+            <div className="btn-container">
+                <button onClick={page !== 1 && decrement} >Previous</button>
+                <button onClick={increment} >Next</button>
+            </div>
+            <div className="movie-container ">
+           {movieDisplay}
+            </div>
+        </div>
+    )
+}
+export default MovieScreen;
